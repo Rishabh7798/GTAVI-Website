b51e77f67fb4aa2da2a4a07a37db5bcf26362220
Rishabh Giri <giririshabh98@gmail.com>
2025-05-19 10:50:48 +0530

    third

--- src/App.jsx
+++ src/App.jsx
@@ -188,21 +188,48 @@ function App() {
                 <h1 className="text-6xl">Not Hunting</h1>
                 <p className="mt-10 text-l font-[Helvetica_Now_Display]">
                   Ahh shit, here we go again... <br />
-                  I didn’t come here to be a legend. I came here to lay low.<br />
-                  But hey, if becoming a legend happens by accident...
-                  Then so be it.
+                  I didn’t come here to be a legend. I came here to lay low.
+                  <br />
+                  But hey, if becoming a legend happens by accident... Then so
+                  be it.
                 </p>
                 <p className="mt-3 text-l font-[Helvetica_Now_Display]">
-                  <b>Trust gets you killed. Love gets you hurt. And being real? That gets you hated.</b> <br />
-                  I’ve seen loyalty fade quicker than brake lights. They come, they go. But I remain — calm in the storm, sharp in the silence. It’s not about being the strongest… It’s about lasting the longest. <br />
-                  Me? I’m not a hero. I’m not a villain. I’m that guy who just wanted a quiet life… then accidentally became a drug kingpin because Google Maps glitched. <br />
-                  Vice City’s got rules… but we don’t follow 'em. We bend 'em, break 'em, and then throw ‘em off a rooftop into a pool party full of influencers and FBI agents. <br />
+                  <b>
+                    Trust gets you killed. Love gets you hurt. And being real?
+                    That gets you hated.
+                  </b>{" "}
+                  <br />
+                  I’ve seen loyalty fade quicker than brake lights. They come,
+                  they go. But I remain — calm in the storm, sharp in the
+                  silence. It’s not about being the strongest… It’s about
+                  lasting the longest. <br />
+                  Me? I’m not a hero. I’m not a villain. I’m that guy who just
+                  wanted a quiet life… then accidentally became a drug kingpin
+                  because Google Maps glitched. <br />
+                  Vice City’s got rules… but we don’t follow 'em. We bend 'em,
+                  break 'em, and then throw ‘em off a rooftop into a pool party
+                  full of influencers and FBI agents. <br />
                 </p>
-                <h1 className="text-2xl">The Sunshine's Fake, But the Trouble’s Real.</h1>
-                <h1 className="text-6xl">gta VI-May 2026</h1>
-                <button className="bg-yellow-500 px-10 py-8 text-black mt-10 text-4xl rounded">
-                  Download Now
-                </button>
+                <h1 className="text-3xl">
+                  The Sunshine's Fake, But the Trouble’s Real.
+                </h1>
+                <h1 className="text-6xl">gta VI - May 2026</h1>
+
+                <div className="mt-10 flex justify-center items-center gap-4">
+                  <button className="bg-yellow-500 px-10 py-6 text-black hover:bg-yellow-400 text-2xl rounded">
+                    Download Now
+                  </button>
+                  {/* <a href="https://github.com/Rishabh7798" target="_blank" rel="noopener noreferrer">
+                    <button className="px-6 py-3 bg-white text-black rounded-full font-bold hover:bg-gray-300 transition text-xl">
+                      Created by Rishabh Giri
+                    </button>
+                  </a> */}
+                  <button className="px-6 py-3 bg-white text-black rounded-full font-bold hover:bg-gray-300 transition text-xl">
+                      Created by Rishabh Giri
+                  </button>
+                </div>
+
+
               </div>
             </div>
           </div>
